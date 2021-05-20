3864c5d1634c63384dd9ac121dab876fb5ad8420
kexin <kexin0614@sjtu.edu.cn>
2021-05-20 11:20:50 +0800

    修正EIDE编译问题

--- HARDWARE/interface/gettaskstatus.h
+++ HARDWARE/interface/gettaskstatus.h
@@ -1,6 +1,8 @@
 #ifndef __GETTASKSTATUS_H__
 #define __GETTASKSTATUS_H__
 
+#include "sys.h"
+
 extern volatile unsigned long long FreeRTOSRunTimeTicks;
 void ConfigureTimeForRunTimeStats(void);
 void TIM5_Init(u16 arr, u16 psc);
